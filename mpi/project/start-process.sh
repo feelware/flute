a3e2ad78d75f9335b688f5093a8f3f48ecc630e9
Stevan <stevanpersonal@gmail.com>
2026-03-03 20:21:32 -0500

    ,,,,,

--- mpi/project/start-process.sh
+++ mpi/project/start-process.sh
@@ -2,4 +2,4 @@
 
 machines
 
-mpirun --allow-run-as-root -np 6 --machinefile /root/machinefile /root/project/process_video $1 $2 $3 $4 > /var/log/mpi_jobs/$1.log 2>&1
+mpirun --allow-run-as-root --mca btl_tcp_if_include eth0 --mca oob_tcp_if_include eth0 --mca routed direct -np 6 --machinefile /root/machinefile /root/project/process_video $1 $2 $3 $4 > /var/log/mpi_jobs/$1.log 2>&1
